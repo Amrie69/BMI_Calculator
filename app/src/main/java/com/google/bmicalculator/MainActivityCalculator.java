3e4fe2b4a901bbfe6fa178a029b1cd4889c81f4e
Amrie Choo II <amrieibifc-wa16@student.tarc.edu.my>
2018-11-10 23:55:17 +0800

    Second Commit

--- app/src/main/java/com/google/bmicalculator/MainActivityCalculator.java
+++ app/src/main/java/com/google/bmicalculator/MainActivityCalculator.java
@@ -1,18 +1,17 @@
 package com.google.bmicalculator;
 
-        import android.content.Intent;
-        import android.support.v7.app.AppCompatActivity;
-        import android.os.Bundle;
-        import android.text.TextUtils;
-        import android.view.View;
-        import android.widget.EditText;
-        import android.widget.ImageView;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivityCalculator extends AppCompatActivity {
 
     public static final String KEY = "com.google.bmicalculator.BMI";
     protected static double BMI;
-    String result;
     double weight;
     double height;
 
@@ -21,10 +20,6 @@ public class MainActivityCalculator extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_calculator);
 
-        Intent intent = new Intent(this, MainActivityResult.class);
-        intent.putExtra(KEY, BMI);
-        intent.putExtra(KEY, result);
-        startActivity(intent);
 
     }
 
@@ -35,33 +30,32 @@ public class MainActivityCalculator extends AppCompatActivity {
         editTextWeight = findViewById(R.id.editTextWeight);
         editTextHeight = findViewById(R.id.editTextHeight);
 
-        if (TextUtils.isEmpty(editTextWeight.getText()) || TextUtils.isEmpty(editTextHeight.getText())) {
-            editTextWeight.setError(getString(R.string.error_weight));
+            if (TextUtils.isEmpty(editTextWeight.getText())){
+                editTextWeight.requestFocus();
+                Toast.makeText(MainActivityCalculator.this, "Please enter your weight",Toast.LENGTH_LONG).show();
 
-        }
+            }
+            else if(TextUtils.isEmpty(editTextHeight.getText())){
+                editTextHeight.requestFocus();
+                Toast.makeText(MainActivityCalculator.this, "Please enter your height",Toast.LENGTH_LONG).show();
 
-        calculateBMI(weight, height);
+            }
+            else {
+                weight = Double.parseDouble(editTextWeight.getText().toString());
+                height = Double.parseDouble(editTextHeight.getText().toString());
+
+                calculateBMI(weight, height);
+                Intent intent = new Intent(this, MainActivityResult.class);
+                intent.putExtra(KEY, BMI);
+                startActivity(intent);
+            }
+
     }
 
     public double calculateBMI(double weight, double height) {
         BMI = weight / (height * height);
         return BMI;
-    }
 
-    public void checkResult(double BMI){
-        ImageView imageViewResult;
-        imageViewResult = findViewById(R.id.imageViewResult);
-
-        if (BMI < 18.5) {
-            imageViewResult.setImageResource(getResources().getIdentifier("bmi_underweight", "drawable", getPackageName()));
-            result = "Underweight. Eat and exercise more";
-        } else if (BMI >= 18.5 && BMI <= 24.9) {
-            imageViewResult.setImageResource(getResources().getIdentifier("bmi_normal", "drawable", getPackageName()));
-            result = "Normal. Keep it up.";
-        } else {
-            imageViewResult.setImageResource(getResources().getIdentifier("bmi_overweight", "drawable", getPackageName()));
-            result = "Overweight. Eat less and exercise more.";
-        }
     }
 
 }
--- app/src/main/java/com/google/bmicalculator/MainActivityResult.java
+++ app/src/main/java/com/google/bmicalculator/MainActivityResult.java
@@ -1,12 +1,12 @@
 package com.google.bmicalculator;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import static com.google.bmicalculator.MainActivityCalculator.BMI;
 
 
 public class MainActivityResult extends AppCompatActivity {
@@ -18,22 +18,27 @@ public class MainActivityResult extends AppCompatActivity {
 
         Intent intent = getIntent();
         if (intent.hasExtra(MainActivityCalculator.KEY)) {
+
             TextView textViewResult;
             textViewResult = findViewById(R.id.textViewResult);
-            String result;
-            result = intent.getStringExtra(MainActivityCalculator.KEY);
-            textViewResult.setText(result);
 
             ImageView imageViewResult;
             imageViewResult = findViewById(R.id.imageViewResult);
 
-
+            if (BMI < 18.5) {
+                imageViewResult.setImageResource(R.drawable.bmi_skinny);
+                textViewResult.setText(getString(R.string.underweight_advice));
+            } else if (BMI >= 18.5 && BMI <= 24.9) {
+                imageViewResult.setImageResource(R.drawable.bmi_normal);
+                textViewResult.setText(getString(R.string.normal_advice));
+            } else {
+                imageViewResult.setImageResource(R.drawable.bmi_overweight);
+                textViewResult.setText(getString(R.string.overweight_advice));
+            }
 
         }
 
     }
-
-
 
 
 }
